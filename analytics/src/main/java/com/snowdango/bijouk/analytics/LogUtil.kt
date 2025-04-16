1f4405e424fdd9fce029fb772465e58f042d3dad
SnowDango <xdi.grand.you.winner@gmail.com>
2025-04-16 15:15:44 +0900

    LogUtilの実装

---
+++ analytics/src/main/java/com/snowdango/bijouk/analytics/LogUtil.kt
@@ -0,0 +1,34 @@
+package com.snowdango.bijouk.analytics
+
+import android.util.Log
+
+class LogUtil {
+
+
+    fun d(tag: String, message: String) {
+        Log.d(tag, message)
+    }
+
+    fun e(tag: String, message: String) {
+        Log.e(tag, message)
+    }
+
+    companion object {
+        private fun getTag(): String {
+            val local = ThreadLocal<String>()
+            return local.get() ?: "LogUtil"
+        }
+
+        fun d(message: String) {
+            val logUtil = LogUtil()
+            logUtil.d(getTag(), message)
+        }
+
+        fun e(message: String) {
+            val logUtil = LogUtil()
+            logUtil.e(getTag(), message)
+        }
+
+    }
+
+}
