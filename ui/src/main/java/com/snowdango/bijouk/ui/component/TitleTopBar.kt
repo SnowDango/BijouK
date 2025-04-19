7dcf5d2795a6937668d7744c7ca25f05d3f85a46
SnowDango <xdi.grand.you.winner@gmail.com>
2025-04-20 03:16:57 +0900

    formatting

--- ui/src/main/java/com/snowdango/bijouk/ui/component/TitleTopBar.kt
+++ ui/src/main/java/com/snowdango/bijouk/ui/component/TitleTopBar.kt
@@ -8,7 +8,6 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TitleTopBar(
