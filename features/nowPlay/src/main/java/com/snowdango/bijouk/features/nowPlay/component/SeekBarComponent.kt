ea7582c5692be48037390624176820167a6b3c6d
SnowDango <xdi.grand.you.winner@gmail.com>
2025-07-20 02:08:59 +0900

    formatting

--- features/nowPlay/src/main/java/com/snowdango/bijouk/features/nowPlay/component/SeekBarComponent.kt
+++ features/nowPlay/src/main/java/com/snowdango/bijouk/features/nowPlay/component/SeekBarComponent.kt
@@ -82,9 +82,9 @@ fun SeekBarComponent(playBackTimeData: PlayBackTimeData?, modifier: Modifier = M
             )
             Text(
                 text = "-" + (
-                        playBackTimeData?.remainingTimeString
-                            ?: stringResource(R.string.time_string_default)
-                        ),
+                    playBackTimeData?.remainingTimeString
+                        ?: stringResource(R.string.time_string_default)
+                    ),
                 style = MaterialTheme.typography.bodyMedium,
                 textAlign = TextAlign.Right,
                 modifier = Modifier
