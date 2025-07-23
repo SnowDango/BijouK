50166221891cd951b887d308adbe43e9b28e9ba4
SnowDango <xdi.grand.you.winner@gmail.com>
2025-07-24 00:29:17 +0900

    queueの取得失敗

--- features/queue/src/main/java/com/snowdango/bijouk/features/queue/QueueScreen.kt
+++ features/queue/src/main/java/com/snowdango/bijouk/features/queue/QueueScreen.kt
@@ -1,9 +1,13 @@
 package com.snowdango.bijouk.features.queue
 
 import android.annotation.SuppressLint
+import android.widget.Toast
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
@@ -18,14 +22,19 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LifecycleEventEffect
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.snowdango.bijouk.features.queue.component.QueueSongCard
 import com.snowdango.bijouk.model.cider.data.QueueData
 import com.snowdango.bijouk.ui.BijouKTheme
+import com.snowdango.bijouk.ui.component.ProgressContent
 import org.koin.compose.viewmodel.koinViewModel
 import org.koin.core.parameter.parametersOf
 
@@ -42,7 +51,19 @@ fun QueueScreen(
         parametersOf(baseUrl, token)
     },
 ) {
+    val context = LocalContext.current
     val queueViewData = viewModel.queueViewDataFlow.collectAsStateWithLifecycle()
+    val isRefreshing = viewModel.isRefreshFlow.collectAsStateWithLifecycle()
+
+    LifecycleEventEffect(Lifecycle.Event.ON_RESUME) {
+        viewModel.queueRefresh()
+    }
+
+    LaunchedEffect(queueViewData.value) {
+        if (queueViewData.value is QueueViewModel.UiState.Error) {
+            Toast.makeText(context, R.string.queue_loading_error_toast, Toast.LENGTH_SHORT).show()
+        }
+    }
 
     Scaffold(
         modifier = modifier,
@@ -77,38 +98,55 @@ fun QueueScreen(
             modifier = modifier
                 .fillMaxSize()
                 .padding(paddingValues),
-            isRefreshing = queueViewData.value?.isRefresh == true,
+            isRefreshing = isRefreshing.value,
             onRefresh = viewModel::queueRefresh,
         ) {
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-            ) {
-                LazyColumn(
-                    modifier = Modifier
-                        .fillMaxSize(),
-                    contentPadding = PaddingValues(
-                        top = 16.dp,
-                        bottom = sheetMinSize,
+            when (queueViewData.value) {
+                is QueueViewModel.UiState.Loading -> {
+                    ProgressContent(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(bottom = sheetMinSize)
                     )
-                ) {
-                    queueViewData.value?.let { viewData ->
-                        viewData.queueDataList.list.forEachIndexed { index, queueData ->
-                            if (queueData.state == QueueData.State.Waiting) {
-                                item {
-                                    QueueSongCard(
-                                        queueData = queueData,
-                                        onClickNext = {
-                                            viewModel.moveQueueNext(index)
-                                        },
-                                        onClickSkip = {
-                                            viewModel.skipQueue(index)
-                                        },
-                                    )
+                }
+
+                is QueueViewModel.UiState.Success -> {
+                    val queueDataList =
+                        (queueViewData.value as QueueViewModel.UiState.Success).queueDataList
+                    Box(modifier = Modifier.fillMaxSize()) {
+                        LazyColumn(
+                            modifier = Modifier.fillMaxSize(),
+                            contentPadding = PaddingValues(
+                                top = 16.dp,
+                            )
+                        ) {
+                            queueDataList.list.forEachIndexed { index, queueData ->
+                                if (queueData.state == QueueData.State.Waiting) {
+                                    item {
+                                        QueueSongCard(
+                                            queueData = queueData,
+                                            onClickNext = {
+                                                viewModel.moveQueueNext(index)
+                                            },
+                                            onClickSkip = {
+                                                viewModel.skipQueue(index)
+                                            },
+                                        )
+                                    }
                                 }
+                            }
+                            item {
+                                Spacer(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .height(sheetMinSize)
+                                )
                             }
                         }
                     }
+                }
+
+                is QueueViewModel.UiState.Error -> {
                 }
             }
         }
