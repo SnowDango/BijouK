3a42ec6cf93f7f3b5c68f0dfcf2e686570cdd756
SnowDango <xdi.grand.you.winner@gmail.com>
2025-07-28 06:23:54 +0900

    Gridのcellを数を幅で決める

--- features/search/src/main/java/com/snowdango/bijouk/features/search/artist/SearchArtistsScreen.kt
+++ features/search/src/main/java/com/snowdango/bijouk/features/search/artist/SearchArtistsScreen.kt
@@ -1,6 +1,6 @@
 package com.snowdango.bijouk.features.search.artist
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -21,11 +21,13 @@ fun SearchArtistsScreen(
     modifier: Modifier = Modifier,
     onClickArtist: (artistId: String) -> Unit,
 ) {
-    Box(
+    BoxWithConstraints(
         modifier = modifier.fillMaxSize()
     ) {
         LazyVerticalGrid(
-            columns = GridCells.Fixed(2),
+            columns = GridCells.Fixed(
+                if (maxWidth < 600.dp) 2 else 3
+            ),
             modifier = Modifier
                 .padding(horizontal = 8.dp)
                 .fillMaxSize(),
