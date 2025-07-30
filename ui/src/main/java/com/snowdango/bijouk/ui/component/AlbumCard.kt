c9324151ea0f1367b92678ab393ecffd38e2db96
SnowDango <xdi.grand.you.winner@gmail.com>
2025-07-31 04:29:36 +0900

    CardThumbRounded

--- ui/src/main/java/com/snowdango/bijouk/ui/component/AlbumCard.kt
+++ ui/src/main/java/com/snowdango/bijouk/ui/component/AlbumCard.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -15,6 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
@@ -51,6 +53,7 @@ fun AlbumCard(
                     modifier = Modifier
                         .fillMaxWidth(fraction = 0.7f)
                         .aspectRatio(1.0f)
+                        .clip(RoundedCornerShape(8.dp))
                 ) {
                     AsyncImage(
                         model = cacheableImageRequest(
