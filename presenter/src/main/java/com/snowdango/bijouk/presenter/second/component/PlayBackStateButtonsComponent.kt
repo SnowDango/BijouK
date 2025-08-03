84ddaf2199ed710d0e1c39fc902747679bcde57a
SnowDango <xdi.grand.you.winner@gmail.com>
2025-08-03 10:41:05 +0900

    formatting

--- presenter/src/main/java/com/snowdango/bijouk/presenter/second/component/PlayBackStateButtonsComponent.kt
+++ presenter/src/main/java/com/snowdango/bijouk/presenter/second/component/PlayBackStateButtonsComponent.kt
@@ -23,7 +23,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.ColorProducer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.snowdango.bijouk.model.cider.data.NowPlayingStatusData
@@ -140,7 +139,6 @@ fun PlayBackStateButtonsComponent(
         }
     }
 }
-
 
 @Preview
 @Composable
